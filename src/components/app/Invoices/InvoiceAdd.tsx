f1240ab88445373e197c5283c912ac6e59128de3
Surojit Paul <surojit99923@gmail.com>
2023-11-20 23:36:26 +0530

    add new line item row.

--- src/components/app/Invoices/InvoiceAdd.tsx
+++ src/components/app/Invoices/InvoiceAdd.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button.tsx";
-import { Settings2Icon, X } from "lucide-react";
+import { ChevronDown, Settings2Icon, X } from "lucide-react";
 import { useNavigate, useParams } from "react-router-dom";
 import {
   Form,
@@ -500,6 +500,29 @@ import {
 } from "@/components/ui/table";
 import { Textarea } from "@/components/ui/textarea.tsx";
 import ItemService from "@/API/Resources/v1/Item/Item.Service.ts";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+} from "@/components/ui/select.tsx";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu.tsx";
+import {
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+} from "@radix-ui/react-dropdown-menu";
 const itemService = new ItemService();
 
 function LineItemInputTable({ taxesDropDown, itemFor, line_items = [] }) {
@@ -512,6 +535,7 @@ function LineItemInputTable({ taxesDropDown, itemFor, line_items = [] }) {
     discount: 0,
     tax: null,
     total: 0,
+    is_loading: false,
   };
   const [lineItems, setLineItems] = useState([BLANK_ROW]);
 
@@ -525,14 +549,17 @@ function LineItemInputTable({ taxesDropDown, itemFor, line_items = [] }) {
     { label: string; value: number }[]
   >([]);
 
-  const itemAutoCompleteFetch = useCallback(async (search_text: string) => {
-    const auto_complete_data = await autoCompleteService.getItems({
-      search_text: search_text,
-      item_for: itemFor,
-    });
-    const { results } = auto_complete_data;
-    return results.map((entry) => ({ label: entry.text, value: entry.id }));
-  }, [itemFor]);
+  const itemAutoCompleteFetch = useCallback(
+    async (search_text: string) => {
+      const auto_complete_data = await autoCompleteService.getItems({
+        search_text: search_text,
+        item_for: itemFor,
+      });
+      const { results } = auto_complete_data;
+      return results.map((entry) => ({ label: entry.text, value: entry.id }));
+    },
+    [itemFor],
+  );
   const handleItemAutoCompleteInitialFocus = useCallback(() => {
     if (isInitialItemLoadingDone) return;
     else {
@@ -552,12 +579,17 @@ function LineItemInputTable({ taxesDropDown, itemFor, line_items = [] }) {
   );
 
   const handleItemSelect = (item_id: number, index: number) => {
-    console.log("item id", item_id);
     const temp_line_item = [...lineItems];
     if (!item_id) {
       temp_line_item[index] = BLANK_ROW;
       setLineItems([...temp_line_item]);
       return;
+    } else {
+      temp_line_item[index] = {
+        ...temp_line_item[index],
+        is_loading: true,
+      };
+      setLineItems([...temp_line_item]);
     }
     // do an item api call.
     itemService
@@ -586,6 +618,7 @@ function LineItemInputTable({ taxesDropDown, itemFor, line_items = [] }) {
                 label: fetched_item.name,
                 value: fetched_item.item_id,
               };
+              item.is_loading = false;
               return item;
             }
             return item;
@@ -594,6 +627,12 @@ function LineItemInputTable({ taxesDropDown, itemFor, line_items = [] }) {
       })
       .catch((error) => console.log(error));
   };
+
+  const handleNewRowAt= (index: number) => {
+    const temp_line_item = [...lineItems];
+    temp_line_item.splice(index + 1, 0, BLANK_ROW);
+    setLineItems([...temp_line_item]);
+  }
   return (
     <div className={"flex flex-col space-y-3 col-span-12"}>
       <ReactSelect
@@ -626,6 +665,9 @@ function LineItemInputTable({ taxesDropDown, itemFor, line_items = [] }) {
             <TableHead className="text-right px-4 py-1 text-xs">
               amount
             </TableHead>
+            <TableHead className={"!border-0"}>
+              &nbsp;
+            </TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
@@ -634,92 +676,131 @@ function LineItemInputTable({ taxesDropDown, itemFor, line_items = [] }) {
               key={index}
               className="divide-x divide-gray-200 hover:bg-none!impotant"
             >
-              <TableCell className="px-1 py-1">
-                <div className="w-full flex flex-col space-y-2" id="item-1">
-                  <ReactAsyncSelect
-                    className={"w-full"}
-                    defaultOptions={itemDefaultList}
-                    inputId={"item"}
-                    loadOptions={handleItemAutoCompleteChange}
-                    onFocus={handleItemAutoCompleteInitialFocus}
-                    placeholder="Type or select an item"
-                    classNames={reactSelectStyle}
-                    components={{
-                      ...reactSelectComponentOverride,
-                    }}
-                    menuPortalTarget={document.body}
-                    isClearable={true}
-                    value={lineItem.item}
-                    onChange={(e_value) => {
-                      handleItemSelect(e_value ? e_value.value : null, index);
-                    }}
-                  />
-                  {lineItem.item && (
-                    <Textarea
-                      className="w-full min-h-[50px]"
-                      placeholder="Item Description"
-                      value={lineItem.description}
-                    />
-                  )}
-                </div>
-              </TableCell>
-              <TableCell className="px-1 py-1 align-top">
-                <div>
-                  <Input
-                    className="w-full border-0 text-right"
-                    id="quantity-1"
-                    value={1.0}
-                  />
-                </div>
-              </TableCell>
-              <TableCell className="px-1 py-1 align-top">
-                <div>
-                  <Input
-                    className="w-full border-0 text-right"
-                    id="price-1"
-                    value={lineItem.price}
-                  />
-                </div>
-              </TableCell>
-              <TableCell className="px-1 py-1 align-top">
-                <div>
-                  <Input
-                    className="w-full border-0 text-right"
-                    id="price-1"
-                    value={0.0}
-                  />
-                </div>
-              </TableCell>
-              <TableCell className="px-1 py-1 align-top">
-                <div>
-                  <ReactSelect
-                    className={"w-full z-100"}
-                    options={taxesDropDown}
-                    inputId={"tax"}
-                    placeholder={"Select tax"}
-                    classNames={reactSelectStyle}
-                    components={{
-                      ...reactSelectComponentOverride,
-                      DropdownIndicator: (props: DropdownIndicatorProps) => {
-                        if (props.selectProps.value) {
-                          return null; // Return null to not display anything when a value is selected
-                        }
-                        return <components.DropdownIndicator {...props} />;
-                      },
-                    }}
-                    menuPortalTarget={document.body}
-                    isClearable={true}
-                    value={lineItem.tax}
-                  />
-                </div>
-              </TableCell>
-              <TableCell className="text-right px-1 py-1 align-top">
-                <div>0</div>
-              </TableCell>
+              {lineItem.is_loading && (
+                <TableCell colSpan={6}>
+                  <div className={"relative h-10 w-full"}>
+                    <LoaderComponent mText={""} />
+                  </div>
+                </TableCell>
+              )}
+              {!lineItem.is_loading && (
+                <>
+                  <TableCell className="px-1 py-1">
+                    <div className="w-full flex flex-col space-y-1" id="item-1">
+                      <ReactAsyncSelect
+                        className={"w-full"}
+                        defaultOptions={itemDefaultList}
+                        inputId={"item"}
+                        loadOptions={handleItemAutoCompleteChange}
+                        onFocus={handleItemAutoCompleteInitialFocus}
+                        placeholder="Type or select an item"
+                        classNames={reactSelectStyle}
+                        components={{
+                          ...reactSelectComponentOverride,
+                        }}
+                        menuPortalTarget={document.body}
+                        isClearable={true}
+                        value={lineItem.item}
+                        onChange={(e_value) => {
+                          handleItemSelect(
+                            e_value ? e_value.value : null,
+                            index,
+                          );
+                        }}
+                      />
+                      {lineItem.item && (
+                        <Textarea
+                          className="w-full min-h-[40px] border-0"
+                          placeholder="Item Description"
+                          value={lineItem.description}
+                        />
+                      )}
+                    </div>
+                  </TableCell>
+                  <TableCell className="px-1 py-1 align-top">
+                    <div>
+                      <Input
+                        className="w-full border-0 text-right"
+                        id="quantity-1"
+                        value={1.0}
+                      />
+                    </div>
+                  </TableCell>
+                  <TableCell className="px-1 py-1 align-top">
+                    <div>
+                      <Input
+                        className="w-full border-0 text-right"
+                        id="price-1"
+                        value={lineItem.price}
+                      />
+                    </div>
+                  </TableCell>
+                  <TableCell className="px-1 py-1 align-top">
+                    <div>
+                      <Input
+                        className="w-full border-0 text-right"
+                        id="price-1"
+                        value={0.0}
+                      />
+                    </div>
+                  </TableCell>
+                  <TableCell className="px-1 py-1 align-top">
+                    <div>
+                      <ReactSelect
+                        className={"w-full z-100"}
+                        options={taxesDropDown}
+                        inputId={"tax"}
+                        placeholder={"Select tax"}
+                        classNames={reactSelectStyle}
+                        components={{
+                          ...reactSelectComponentOverride,
+                          DropdownIndicator: (
+                            props: DropdownIndicatorProps,
+                          ) => {
+                            if (props.selectProps.value) {
+                              return null; // Return null to not display anything when a value is selected
+                            }
+                            return <components.DropdownIndicator {...props} />;
+                          },
+                        }}
+                        menuPortalTarget={document.body}
+                        isClearable={true}
+                        value={lineItem.tax}
+                      />
+                    </div>
+                  </TableCell>
+                  <TableCell className="text-right px-1 py-1 align-top">
+                    <div>0</div>
+                  </TableCell>
+                </>
+              )}
             </TableRow>
           ))}
         </TableBody>
       </Table>
+      <div className="flex mt-2.5">
+        <Button variant="default" className={"border-r-0 rounded-r-none"} type={"button"}
+        onClick={()=>handleNewRowAt(lineItems.length-1)}
+        >
+          Add New Row
+        </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="default"
+              size={"icon"}
+              className={"ml-0.5 border-l-0 rounded-l-none"}
+              type={"button"}
+            >
+              <ChevronDown />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-36">
+            <DropdownMenuItem>Bulk Insert</DropdownMenuItem>
+            <DropdownMenuItem>Insert Header</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>{" "}
     </div>
   );
 }
